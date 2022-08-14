e76694f8b2291f0ac32d388eaf5d724185251cb8
hieund20 <hieund898@gmail.com>
2022-08-15 01:22:33 +0700

    Add header

--- nomfe/src/app/app.module.ts
+++ nomfe/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MdbCollapseModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
